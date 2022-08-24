6f0d5161cae3520c95ffb67e6da54ad31edc83b5
Cleverson Guandalin <clever@unochapeco.edu.br>
2022-08-23 23:55:10 -0300

    Prática 1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,19 @@
+let nome
+let email
+let profissao
+let idade
+let lugar
+
+nome = "clever"
+email = "clever@unochapeco.edu.br"
+profissao = 'Engenheiro'
+idade = 30
+lugar = 'Mexico'
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+
+console.log('Olá, '+ nome + '. Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ' + lugar + '. Confirma pra gente alguns dados? A sua idade é ' + idade + ', você é ' + profissao + ' e o seu email é '+ email + '? Ficamos no aguardo. Muito obrigado e boa viagem! :D')
